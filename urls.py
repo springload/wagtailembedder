7bc7702bad3cdfaea35c5473f83476d70487067e
jordij <jordi.joan@gmail.com>
2015-01-16 14:28:59 +1300

    First working version

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from wagtailembedder.views import chooser
-
-
-urlpatterns = [
-    url(r'^chooser/snippet_types/$', chooser.index, name='wagtailembedder_class_list'),
-    url(r'^chooser/snippet_objects/(\w+)/(\w+)/$', chooser.index_objects, name='wagtailembedder_class_chosen'),
-]
