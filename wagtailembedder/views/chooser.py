f4d022913017a9736093bccd21bdcdf266116f86
Loic Teixeira <loic@springload.co.nz>
2016-04-26 16:45:49 +1200

    Make meta description optional
    Contribute to #3

--- wagtailembedder/views/chooser.py
+++ wagtailembedder/views/chooser.py
@@ -22,7 +22,7 @@ def index(request):
             content_type._meta.app_label,
             content_type._meta.model.__name__,
             content_type._meta.verbose_name,
-            content_type._meta.description,
+            getattr(content_type._meta, 'description', ''),
         )
         for content_type in get_snippet_models()
         if user_can_edit_snippet_type(request.user, content_type)
