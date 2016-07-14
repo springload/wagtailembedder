542aa6c29c589da52392a31416d6e079bcc079f5
Loic Teixeira <loic@springload.co.nz>
2016-07-14 14:33:10 +1200

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='wagtailembedder',
-    version='1.0',
+    version='1.1',
     packages=['wagtailembedder'],
     include_package_data=True,
     license='BSD License',
